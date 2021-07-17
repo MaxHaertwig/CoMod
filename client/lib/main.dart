05e520b536eda0a077ae4dc7b86b8c0056302313
Max Härtwig <max.haertwig@mailbox.tu-dresden.de>
2021-07-17 12:27:34 +0200

    Let client app communicate with websocket

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,19 +13,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Client'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Hello world!'),
-            ],
-          ),
-        ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final textEditingController = TextEditingController();
+  final channel = WebSocketChannel.connect(Uri.parse('ws://localhost:3000'));
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Client'),
       ),
+      body: Center(
+          child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: textEditingController,
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {
+                if (!textEditingController.text.isEmpty) {
+                  channel.sink.add(textEditingController.text);
+                }
+              },
+              child: const Text("Send"),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
