16715997316d8258fa84a04a334eb6c634eafb34
Max Härtwig <max.haertwig@mailbox.tu-dresden.de>
2021-08-11 17:27:49 +0200

    Design collaboration protocol

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import * as Koa from 'koa';
 import * as websockify from 'koa-websocket';
-import {CollaborationMessage} from './pb/collaboration_pb';
+import { CollaborationRequest } from './pb/collaboration_pb';
 
 const wsOptions = {};
 const app = websockify(new Koa(), wsOptions);
@@ -8,8 +8,8 @@ const app = websockify(new Koa(), wsOptions);
 app.ws.use(ctx => {
   ctx.websocket.binaryType = 'arraybuffer';
   ctx.websocket.on('message', (buffer: Buffer) => {
-    const message = CollaborationMessage.deserializeBinary(buffer);
-    console.log(`Received message: ${message.getMessage()}`);
+    const message = CollaborationRequest.deserializeBinary(buffer);
+    console.log(`Received message: ${message}`);
   });
 });
 
