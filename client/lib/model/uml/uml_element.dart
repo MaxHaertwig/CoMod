5c39087f628022b264ebb0e4754623d06c999fa7
Max Härtwig <max.haertwig@mailbox.tu-dresden.de>
2021-10-03 13:25:36 +0200

    Fix doc comments

--- client/lib/model/uml/uml_element.dart
+++ client/lib/model/uml/uml_element.dart
@@ -1,15 +1,15 @@
 import 'package:tuple/tuple.dart';
 
+/// A UML element.
 abstract class UMLElement {
   String get id => '';
 
-  // TODO: doc
-  // Instructs the element to update its attributes, insert element, and delete elements. Returns the elements that were created as part of the update.
+  /// Instructs the element to update its attributes, insert element, and delete elements. Returns the elements that were created as part of the update.
   List<UMLElement>? update(List<Tuple2<String, String>> attributes,
       List<String> addedElements, List<String> deletedElements) {}
 }
 
-// A UMLElement with a name.
+/// A UMLElement with a name.
 abstract class NamedUMLElement extends UMLElement {
   String get name => '';
   set name(String newName) {}
