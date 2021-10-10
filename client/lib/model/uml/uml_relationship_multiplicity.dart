cbf7975eafc196e663c80ade7960559298372d84
Max Härtwig <max.haertwig@mailbox.tu-dresden.de>
2021-10-10 14:45:34 +0200

    Fix MultiplicityControl bug

--- client/lib/model/uml/uml_relationship_multiplicity.dart
+++ client/lib/model/uml/uml_relationship_multiplicity.dart
@@ -12,11 +12,17 @@ class UMLRelationshipMultiplicity {
       return UMLRelationshipMultiplicity.empty();
     }
     if (!string.contains('..')) {
-      return UMLRelationshipMultiplicity(parseComponent(string), -1);
+      final component = parseComponent(string);
+      return component == -99
+          ? UMLRelationshipMultiplicity(-1, component)
+          : UMLRelationshipMultiplicity(component, -1);
     }
     final components = string.split('..');
-    return UMLRelationshipMultiplicity(
-        parseComponent(components[0]), parseComponent(components[1]));
+    final lower = parseComponent(components[0]);
+    final upper = parseComponent(components[1]);
+    return lower == -1 && upper != -1
+        ? UMLRelationshipMultiplicity(upper, -1)
+        : UMLRelationshipMultiplicity(lower, upper);
   }
 
   static int parseComponent(String component) {
--- client/test/model/uml/uml_relationship_multiplicity_test.dart
+++ client/test/model/uml/uml_relationship_multiplicity_test.dart
@@ -2,6 +2,24 @@ import 'package:client/model/uml/uml_relationship_multiplicity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test('UMLRelationshipMultiplicity parse', () {
+    final multiplicity1 = UMLRelationshipMultiplicity.parse('');
+    expect(multiplicity1.lower, -1);
+    expect(multiplicity1.upper, -1);
+
+    final multiplicity2 = UMLRelationshipMultiplicity.parse('1');
+    expect(multiplicity2.lower, 1);
+    expect(multiplicity2.upper, -1);
+
+    final multiplicity3 = UMLRelationshipMultiplicity.parse('*');
+    expect(multiplicity3.lower, -1);
+    expect(multiplicity3.upper, -99);
+
+    final multiplicity4 = UMLRelationshipMultiplicity.parse('1..2');
+    expect(multiplicity4.lower, 1);
+    expect(multiplicity4.upper, 2);
+  });
+
   test('UMLRelationshipMultiplicity isEmpty', () {
     expect(UMLRelationshipMultiplicity.empty().isEmpty, true);
     expect(UMLRelationshipMultiplicity(1, 1).isEmpty, false);
@@ -24,7 +42,7 @@ void main() {
     expect(UMLRelationshipMultiplicity(1, 1).xmlRepresentation, '1');
     expect(UMLRelationshipMultiplicity(1, 2).xmlRepresentation, '1..2');
     expect(UMLRelationshipMultiplicity(1, -99).xmlRepresentation, '1..*');
-    expect(UMLRelationshipMultiplicity(-99, -1).xmlRepresentation, '*');
+    expect(UMLRelationshipMultiplicity(-1, -99).xmlRepresentation, '*');
     expect(UMLRelationshipMultiplicity(-99, -99).xmlRepresentation, '*');
   });
 }
